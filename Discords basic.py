8ad73811b37c04a38c1d35e3d65f53caa42bebe7
BenteTheWagon <33541473+BenteTheWagon@users.noreply.github.com>
2020-01-08 09:01:47 +0100

    Update Discords basic.py

--- Discords basic.py
+++ Discords basic.py
@@ -1,1 +1,20 @@
-print yeet
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(Jeres egen discord bot token)
+
+
+
+#Botten kræver at i downloader dotenv.py og discord.py
+#i kan download det ved at gøre følgende: pip install -U python-dotenv og pip install -U discord.py
